76e40c562f97c5f19294782d083107e242b4256a
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-04-01 11:05:46 +0530

    update user/urls.py

--- library_managment_system/user/urls.py
+++ library_managment_system/user/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home_page, name='home'),
     path('register/', views.register_page, name='register_page'),
     path('login/', views.login_page, name='login_page'),
     path('logout/', views.logout_page, name='logout_page'),
+    path('book/<int:book_id>/', views.book_page, name='book_page'),
+    path('book/create/', views.create_book_page, name='create_book_page'),
 ]
