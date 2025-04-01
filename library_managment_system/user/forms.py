e6ebdd42943ea4b1d9593c9a4985673a3851c876
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-04-01 11:06:13 +0530

    update user/forms.py

--- library_managment_system/user/forms.py
+++ library_managment_system/user/forms.py
@@ -3,6 +3,13 @@ from .models import Book
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
+class Book_Form(forms.ModelForm):
+
+    class Meta:
+
+        model = Book
+        fields = ('book_name', 'cover_image', 'issued_date', 'auther', 'category')
+
 class Member_Form(UserCreationForm):
 
     class Meta:
