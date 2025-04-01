82ee035ddb2a56d83f7a91944d5ecd42469ae371
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-04-01 11:05:58 +0530

    update user/models.py

--- library_managment_system/user/models.py
+++ library_managment_system/user/models.py
@@ -6,9 +6,13 @@ class Book(models.Model):
     categories = [('ACT', 'Action'), ('ROM', 'Romance'), ('MYS', 'Mystries'), ('THR', 'Thrill'), ('HOR', 'Horror')]
 
     book_name = models.CharField(max_length=100)
+    cover_image = models.ImageField(upload_to='covers/')
     issued_date = models.DateField()
     auther = models.CharField(max_length=50)
     category = models.CharField(max_length=3, choices=categories, default='HOR')
+
+    def __str__(self):
+        return self.book_name
 
 # class Member(models.Model):
 
